c0902c6b821320ce9c3264aabfed42154fe8e61a
princeakshay27 <princeakshay27@gmail.com>
2018-02-18 01:00:56 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -49,7 +49,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one', function (req, res) {
-  res.sendFile(createTemplate(articleOne));
+  res.send(createTemplate(articleOne));
 });
 
 app.get('/article-two', function (req, res) {
