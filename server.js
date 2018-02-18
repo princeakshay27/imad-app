a7159743a0d9f40ef3dd3ff37c1cedf1d4f20710
princeakshay27 <princeakshay27@gmail.com>
2018-02-18 18:57:01 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -67,22 +67,6 @@ app.get('/:articleName', function (req, res) {
   res.send(createTemplate(articles[articleName]));
 });
 
-app.get('/article-two', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
-});
-
-app.get('/article-three', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
-});
-
-app.get('/article-two', function (req, res) {
-  res.send('article two is requested and will be serve');
-});
-
-app.get('/article-three', function (req, res) {
-  res.send('article three is requested and will be serve');
-});
-
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
