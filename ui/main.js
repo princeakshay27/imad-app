864a20d92cb17b563d3e4090a1bf9a7f91a9f0ea
princeakshay27 <princeakshay27@gmail.com>
2018-02-18 22:59:25 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,7 +4,7 @@ element.innerHTML='akshay sinha';
 var img = document.getElementById('madi');
 var marginLeft=0;
 function moveRight(){
-    marginLeft=marginlleft+1;
+    marginLeft=marginleft+1;
     img.style.marginLeft=marginLeft+'px';
 }
 img.onclick = function(){
